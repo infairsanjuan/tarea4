89005f1f158ebe767775dc9f2e7ffa1080b7425e
antonio romero <antoniojoseromeromendez@gmail.com>
2023-03-05 12:39:32 +0100

    version 1.2

--- src/numerosprimos/CribaEratostenes.java
+++ src/numerosprimos/CribaEratostenes.java
@@ -1,83 +1,111 @@
 /**
  * Clase para generar todos los números primos de 1 hasta
- * un número máximo especificado por el usuario. Como 
+ * un número máximo especificado por el usuario. Como
  * algoritmo se utiliza la criba de Eratóstenes.
- * 
- * Eratóstenes de Cirene (276 a.C., Cirene, Libia – 194
- * a.C., Alejandría, Egipto) fue el primer hombre que 
- * calculó la circunferencia de la Tierra. También
- * se le conoce por su trabajo con calendarios que ya 
- * incluían años bisiestos y por dirigir la mítica 
- * biblioteca de Alejandría.
- * 
  * El algoritmo es bastante simple: Dado un vector de
  * enteros empezando en 2, se tachan todos los múltiplos
  * de 2. A continuación, se encuentra el siguiente
- * entero no tachado y se tachan todos sus múltiplos. El 
+ * entero no tachado y se tachan todos sus múltiplos. El
  * proceso se repite hasta que se pasa de la raíz cuadrada
- * del valor máximo. Todos los números que queden sin 
+ * del valor máximo. Todos los números que queden sin
  * tachar son números primos.
- */ 
-
-package numerosprimos;    
-    
-public class CribaEratostenes 
-{ 
- /** 
- * Generar números primos de 1 a max
- * @param max es el valor máximo
- * @return Vector de números primos
  */
- public static int[] generarPrimos (int max) 
- { 
- int i,j; 
- if (max >= 2) { 
- 
- // Declaraciones
- int dim = max + 1; // Tamaño del array 
- boolean[] esPrimo = new boolean[dim]; 
- 
- // Inicializar el array
- for (i=0; i<dim; i++) 
- esPrimo[i] = true; 
- 
- // Eliminar el 0 y el 1, que no son primos
- esPrimo[0] = esPrimo[1] = false; 
- 
- // Criba
- for (i=2; i<Math.sqrt(dim)+1; i++) { 
- if (esPrimo[i]) {
- // Eliminar los múltiplos de i
- for (j=2*i; j<dim; j+=i) 
- esPrimo[j] = false; 
- } 
- }
- 
- // ¿Cuántos primos hay? 
- int cuenta = 0; 
- for (i=0; i<dim; i++) { 
- if (esPrimo[i]) 
- cuenta++; 
- } 
- // Rellenar el vector de números primos
- int[] primos = new int[cuenta]; 
- for (i=0, j=0; i<dim; i++) { 
- if (esPrimo[i])
- primos[j++] = i; 
- } 
- 
- return primos; 
- 
- } else { // max < 2 
- 
- return new int[0]; // Vector vacío
- } 
- }
+package numerosprimos;
+
+public class CribaEratostenes {
+
+    /**
+     *
+     * Genera un array de números primos desde 1 hasta el valor máximo dado.
+     *
+     * @param maximoValor el valor máximo hasta el cual se generarán los números
+     * primos.
+     *
+     * @return un array de números primos.
+     */
+    public static int[] generarPrimos(int maximoValor) {
+        int[] primos;
+        if (maximoValor < 2) {
+            primos = new int[0]; // vacío
+        } else {
+            boolean[] esPrimo = inicializarArray(maximoValor);
+            criba(maximoValor, esPrimo);
+            primos = obtenerNumerosPrimos(maximoValor, esPrimo);
+        }
+        return primos;
+    }
+
+    /**
+     *
+     * Inicializa un array de tamaño max+1, donde todos los valores son true,
+     * excepto el primer y segundo elemento que
+     *
+     * @param max el tamaño del array a crear.
+     *
+     * @return un array inicializado con todos los valores en true.
+     */
+    private static boolean[] inicializarArray(int max) {
+        int dim = max + 1;
+        boolean[] esPrimo = new boolean[max + 1];
+
+        for (int i = 0; i < dim; i++) {
+            esPrimo[i] = true;
+        }
+
+        esPrimo[0] = esPrimo[1] = false;
+
+        return esPrimo;
+    }
+
+    /**
+     *
+     * Elimina los valores del array que no son primos, utilizando la técnica de
+     * la Criba de Eratóstenes.
+     *
+     * @param max el valor máximo hasta el cual realizar la criba.
+     *
+     * @param esPrimo el array a cribar.
+     */
+    private static void criba(int max, boolean[] esPrimo) {
+        int dim = max + 1;
+
+        for (int i = 2; i < Math.sqrt(dim) + 1; i++) {
+            if (esPrimo[i]) {
+                for (int j = 2 * i; j < dim; j += i) {
+                    esPrimo[j] = false;
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     * Obtener un array con los valores que son primos.
+     *
+     * @param max el valor máximo hasta el cual se generaron los números primos.
+     *
+     * @param esPrimo el array con los valores que se determinaron como primos.
+     *
+     * @return un array con los valores que son primos.
+     */
+    private static int[] obtenerNumerosPrimos(int max, boolean[] esPrimo) {
+        int dim = max + 1;
+        int cuenta = 0;
+
+        for (int i = 0; i < dim; i++) {
+            if (esPrimo[i]) {
+                cuenta++;
+            }
+        }
+
+        int[] primos = new int[cuenta];
+
+        for (int i = 0, j = 0; i < dim; i++) {
+            if (esPrimo[i]) {
+                primos[j++] = i;
+            }
+        }
+
+        return primos;
+    }
 }
-
-
-
-
-
-
-
--- src/numerosprimos/NumerosPrimos.java
+++ src/numerosprimos/NumerosPrimos.java
@@ -1,27 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
 package numerosprimos;
 
-/** Clase para probar el algoritmo de la criba de Eratóstenes 
- *  para generar números primos.
- * 
+/**
+ * Clase para probar el algoritmo de la criba de Eratóstenes para generar
+ * números primos.
+ *
  * @author arkano
  */
-public class NumerosPrimos {   
-    
+public class NumerosPrimos {
+
     public static void main(String[] args) {
-       int[] centArray = CribaEratostenes.generarPrimos(100);             // Probamos a generar los primos entre 1 y 100
-       for (int i=0;i<centArray.length;i++){
+        int[] centArray = CribaEratostenes.generarPrimos(1);   // Probamos a generar los primos entre 1 y 100
+        for (int i = 0; i < centArray.length; i++) {
             System.out.println(centArray[i]);                   // Los mostramos
-            
+
         }
-    
+
+        CribaEratostenes.generarPrimos(100);
+
     }
 }
-    
-    
- 
-
